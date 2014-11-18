b1a9a57db35afbd25426af325774c098298351fa
Mello <dan.mello1001@gmail.com>
2014-11-18 11:36:08 -0700

    completed exercise

--- app/models/person.rb
+++ app/models/person.rb
@@ -4,4 +4,9 @@ class Person < ActiveRecord::Base
     "#{first_name} #{last_name}"
   end
 
+  has_many :employments
+  has_many :job_titles, through: :employments
+  has_many :locations, through: :employments
+  has_many :organizations, through: :employments
+
 end
